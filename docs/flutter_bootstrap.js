b4c97a3cb98b4f80593863f002c01a06aa089506
guillelpz <guillelpz@correo.ugr.es>
2025-09-24 01:13:01 +0200

    Calendario por tarea añadido

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "387684569"
+    serviceWorkerVersion: "3965506770"
   }
 });
