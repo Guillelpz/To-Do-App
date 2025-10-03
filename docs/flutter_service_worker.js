e324e487f95dd56ab1c3aadd1cb136baa8d13401
guillelpz <guillelpz@correo.ugr.es>
2025-10-03 20:54:37 +0200

    Calendario con localizador de día

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -22,7 +22,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "50a46547d4b37781259cc3387d058669",
+"flutter_bootstrap.js": "b5ac44e17a2fa57d7cf42acffc08c1f6",
 "IconLaura16.png": "e83b407b029db1acedeec028ff33467a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/IconLaura512.png": "aad313014b36b859d97b6d228fb3dd75",
 "index.html": "3cbf59b3b0733d44c03ebb0a328208dc",
 "/": "3cbf59b3b0733d44c03ebb0a328208dc",
-"main.dart.js": "b3b2d83fc6b8693db476bcd9c35f7651",
+"main.dart.js": "e794858a7ecdee6750dc5d952ae64ea9",
 "manifest.json": "794f43275d3c04d7fc5a82e1d920b04f",
 "version.json": "72f0c0773dadcfabe514f56dd5c8c930"};
 // The application shell files that are downloaded before a service worker can
